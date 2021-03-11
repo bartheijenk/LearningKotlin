651c4e28bc0a1e67a778a0bc16085a6f746ae519
bartheijenk <bdheijenk@gmail.com>
2021-03-11 09:35:49 +0100

    First Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val scanner = Scanner(System.`in`)
+        while (true) {
+            var input = scanner.next()
+
+            println("Input: " + input)
+        }
+    }
+
+    var hoi : String = ""
+        private set
+}
